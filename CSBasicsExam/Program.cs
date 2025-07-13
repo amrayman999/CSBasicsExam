13234ce428388cb83c31dae363b4aeb24ef6984b
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-07-13 18:02:32 +0300

    Add project files.

---
+++ CSBasicsExam/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CSBasicsExam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        #region Question 1
+
+        #endregion
+        #region Question 2
+    }
+}
